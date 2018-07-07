db3ec2b0deca43a10a5f23bb7c5c93d9ba6e2e17
RangerX-36917 <chrischen369@outlook.com>
2018-07-07 17:10:47 +0800

    Finish data_export
    Finish exporting data in forms of array  of Strings to .csv file. Need interface of extracting real data to proceed.

--- src/repastcity3/main/DataLogger.java
+++ src/repastcity3/main/DataLogger.java
@@ -3,6 +3,7 @@ package repastcity3.main;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 import org.apache.commons.csv.CSVFormat;
@@ -10,32 +11,61 @@ import org.apache.commons.csv.CSVPrinter;
 
 import repast.simphony.util.collections.IndexedIterable;
 import repastcity3.agent.IAgent;
+import repastcity3.environment.Farm;
 import repastcity3.environment.FixedGeography;
 
 public class DataLogger {
+	char RecordSeparator;
 	public DataLogger () {
-		
+		RecordSeparator = '\n';
 	}
-
+	public DataLogger (char s) {
+		RecordSeparator = s;
+	}
+	
 	public <T> void printData(IndexedIterable<T> agentList) throws IOException {
-		System.out.println("start to export data to csv file");
-		final String fileName = "data.csv";
-		final char RecordSeparator = '\n';
+		T t = agentList.get(0);
+		if( t instanceof Farm) {
+			//System.out.println("exporting farm data");
+			String fileName1 = "./Output/farm_calorie_production.csv";
+			String fileName2 = "./Output/farm_organic_waste.csv";
+			String fileName3 = "./Output/agent_waste.csv";
+			ArrayList<String> list1 = new ArrayList<>();
+			ArrayList<String> list2 = new ArrayList<>();
+			ArrayList<String> list3 = new ArrayList<>();
+			
+			String x;
+			for (int i = 0; i < agentList.size(); i++) {
+				x = agentList.get(i).toString();
+				list1.add((x));
+			}
+			
+			printDataToFile(list1, fileName1);
+		} else if (t instanceof IAgent) {
+			//System.out.println("exporting agent data");
+			String fileName1 = "./Output/agent_calorie_consumption.csv";
+			String fileName2 = "./Output/agent_waste.csv";
+			ArrayList<String> list1 = new ArrayList<>();
+			ArrayList<String> list2 = new ArrayList<>();
+			
+			String x;
+			for (int i = 0; i < agentList.size(); i++) {
+				x = agentList.get(i).toString();
+				list1.add((x));
+			}
+			
+			printDataToFile(list1, fileName1);
+		}
+		//System.out.println("start to export data to csv file");
+	}
+	
+	public void printDataToFile(ArrayList<String> dataList, String fileName) throws IOException {
 		CSVFormat formator = CSVFormat.DEFAULT.withRecordSeparator(RecordSeparator);
 		Writer fileWriter = new FileWriter(fileName,true);
 		CSVPrinter printer = new CSVPrinter(fileWriter, formator);
 		
-		ArrayList<String> list = new ArrayList<>();
-		String x;
-		for (int i = 0; i < agentList.size(); i++) {
-			x = agentList.get(i).toString()+i;
-			list.add((x));
-		}
-		printer.printRecord(list);
+		printer.printRecord(dataList);
 		fileWriter.close();
 		printer.close();
-	
-	
 	}
-
 }
